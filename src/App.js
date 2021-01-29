2ccb77849615d7a878cc8c5c54efbb817a6b244c
Dusan M <dusan@musth.io>
2021-01-29 07:41:08 -0500

    Building our React App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 import Routes from "./Routes";
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
+import { AppContext } from "./libs/contextLib";
 function App() {
-  return (
-      <div className="App container py-3">
-          <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-              <LinkContainer to="/">
-                  <Navbar.Brand className="font-weight-bold text-muted">Scratch</Navbar.Brand>
-              </LinkContainer>
-              <Navbar.Toggle />
-              <Navbar.Collapse className="justify-content-end">
-                  <Nav activeKey={window.location.pathname}>
-                      <LinkContainer to="/signup">
-                          <Nav.Link>Signup</Nav.Link>
-                      </LinkContainer>
-                      <LinkContainer to="/login">
-                          <Nav.Link>Login</Nav.Link>
-                      </LinkContainer>
-                  </Nav>
-              </Navbar.Collapse>
-          </Navbar>
-          <Routes />
-      </div>
-  );
+    const [isAuthenticated, userHasAuthenticated] = useState(false);
+    function handleLogout() {
+      userHasAuthenticated(false);
+      }
+    return (
+        <div className="App container py-3">
+            <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+                <LinkContainer to="/">
+                    <Navbar.Brand className="font-weight-bold text-muted">Scratch</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav activeKey={window.location.pathname}>
+                    {isAuthenticated ? (
+<Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+) : (
+<>
+<LinkContainer to="/signup">
+<Nav.Link>Signup</Nav.Link>
+</LinkContainer>
+<LinkContainer to="/login">
+<Nav.Link>Login</Nav.Link>
+</LinkContainer>
+</>
+)}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+                <Routes />
+            </AppContext.Provider>
+        </div>
+    );
 }
-export default App;
+export default App; 
